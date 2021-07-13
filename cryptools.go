86330176bde1cb57ff21c10edda9bb9c94f04760
Frederico Winter <frdw@protonmail.com>
2021-07-13 13:34:43 -0300

    added base64 encode and decode functions (shortcut do encoding/base64 go library)

--- cryptools.go
+++ cryptools.go
@@ -3,6 +3,8 @@
 package cryptools
 
 import (
+	"encoding/base64"
+	"log"
 	"strings"
 	"unicode"
 )
@@ -72,4 +74,23 @@ func NewRot13() Cipher {
 // NewCaesar returns a new Shift Cipher with 3 as distance.
 func NewCaesar() Cipher {
 	return NewShift(3)
+}
+
+// Base64 encoding tools
+func NewB64() Cipher {
+	return B64{}
+}
+
+type B64 struct{}
+
+func (b B64) Encode(s string) string {
+	return base64.StdEncoding.EncodeToString([]byte(s))
+}
+
+func (b B64) Decode(s string) string {
+	dec, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(dec)
 }
