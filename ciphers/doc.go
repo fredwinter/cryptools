c1082afb88e940d367c59b1c6bb4199b25df05e6
Frederico Winter <frdw@protonmail.com>
2021-07-19 21:32:51 -0300

    fixed docstring of package ciphers

--- ciphers/doc.go
+++ ciphers/doc.go
@@ -1,3 +1,3 @@
-/*Package cryptools/ciphers has some functions to do basic encryption using simple ciphers.
+/*Package ciphers has some functions to do basic encryption using simple ciphers.
 I may not have the best coding skills, but I really enjoyed coding it.*/
 package ciphers
